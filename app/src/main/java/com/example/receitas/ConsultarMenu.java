51e705d2a602763505cffdbee56ea6c63ac725d9
ana-luizaa <ana.guglielmini@etec.sp.gov.br>
2021-09-25 19:47:04 -0300

    Criando telas

---
+++ app/src/main/java/com/example/receitas/ConsultarMenu.java
@@ -0,0 +1,14 @@
+package com.example.receitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsultarMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_menu);
+    }
+}
---
+++ app/src/main/java/com/example/receitas/ConsultarReceitas.java
@@ -0,0 +1,14 @@
+package com.example.receitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsultarReceitas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_receitas);
+    }
+}
