c7ab77fb717483a1e0a54fc9ce4d23c217aea361
ana-luizaa <ana.guglielmini@etec.sp.gov.br>
2021-10-16 17:23:42 -0300

    layout tela inicial feita

---
+++ app/src/main/java/com/example/receitas/ListarMinhasReceitas.java
@@ -0,0 +1,14 @@
+package com.example.receitas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListarMinhasReceitas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_minhas_receitas);
+    }
+}
